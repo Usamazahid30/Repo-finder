1e5bd2e8b3225d563cab3ba928b8b16ab1e91f9c
usama_zahid <Usamazahid30@users.noreply.github.com>
2021-07-05 15:01:50 +0500

    repo finder using API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
 function App() {
+  const [repos, setRepos] = useState([{}]);
+
+
+  useEffect(() => {
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/usamazahid30/repos")
+      const data = await response.json();
+      console.log(data)
+      setRepos(data)
+    }
+
+    getRepos();
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All repository</h1>
+      <ul className="list">
+        {repos.map((repoObj, ind) => {
+          return (
+            <li key={ind}>{repoObj.name} </li>
+          )
+        })}
+      </ul>
     </div>
+
+
   );
 }
 
